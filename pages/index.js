73c6627109c1a1526f4a99925582dff25b58d821
Tharunkumar001 <tharung1411@gmail.com>
2021-12-14 10:34:49 +0530

    ui updation

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,10 @@ import axios from 'axios';
 import cogoToast from 'cogo-toast';
 
 export default function Home() {
-  const [data,setData] = useState({name:"",rollNo:"",class:"NA"});
-  const [open,setOpen] = useState(false);
-  const [error,setError] = useState();
-  const [loading,setLoading] = useState(false);
+  const [data, setData] = useState({ name: "", rollNo: "", class: "NA" });
+  const [open, setOpen] = useState(false);
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   const handleClose = () => {
@@ -31,27 +31,27 @@ export default function Home() {
     e.preventDefault();
     let rollNoLength = data.rollNo.length;
 
-    const validateRollNo = (rollNoLength === 8) ?  false:  true;
-    const validateclass = (data.class !== "NA")? false: true;
+    const validateRollNo = (rollNoLength === 8) ? false : true;
+    const validateclass = (data.class !== "NA") ? false : true;
 
-    if(!validateRollNo && !validateclass){
-      axios.post("http://localhost:3000/api/formHandler",data).then((res) => {
-      //https://pylamp-domain-realm.vercel.app/  
-      setLoading(true);
-      if(res.data == false){
+    if (!validateRollNo && !validateclass) {
+      axios.post("https://pylamp-domain-realm.vercel.app/api/formHandler", data).then((res) => {
+        //https://pylamp-domain-realm.vercel.app/  
+        setLoading(true);
+        if (res.data == false) {
           setLoading(false);
-          cogoToast.info("already you marked your attendance",{position:'bottom-center'});
+          cogoToast.info("already you marked your attendance", { position: 'bottom-center' });
 
           setTimeout(() => {
             handleOpen();
-          },3000);
+          }, 3000);
 
-        }else{
+        } else {
           setLoading(false);
           handleOpen();
         }
       })
-    }else{
+    } else {
       cogoToast.info("Enter valid RollNo or Class");
     }
   }
@@ -72,33 +72,33 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to Pylamp!
         </h1>
-        
+
         <h3>Domain Realm Series - II</h3>
 
         <p className={styles.description}>
           We Are Here For You{' '}<br />
-          <Image src={Logo} alt="logo" width="152rem" height="152rem"/>
+          <Image src={Logo} alt="logo" width="152rem" height="152rem" />
         </p>
 
         <div className={styles.grid}>
           <label>
             <form className={styles.formLabel} onSubmit={submitHandler}>
-              <input placeholder="Name" autoComplete="off" value={data.name.trim()} type="text" name="inputForName" required  className={styles.input}
-                onChange={e => setData({...data, name:e.target.value})}
+              <input placeholder="Name" autoComplete="off" value={data.name.trim()} type="text" name="inputForName" required className={styles.input}
+                onChange={e => setData({ ...data, name: e.target.value })}
               /><br />
 
-              <input placeholder="RollNo"  autoComplete="off" value={data.rollNo.trim()} type="text" name="inputForName" required  className={styles.input}
-                onChange={e => setData({...data, rollNo:e.target.value})}
+              <input placeholder="RollNo" autoComplete="off" value={data.rollNo.trim()} type="text" name="inputForName" required className={styles.input}
+                onChange={e => setData({ ...data, rollNo: e.target.value })}
               /><br />
               {/* {error && <span>Enter your Full Pattern</span>} */}
-              <select  className={styles.input} 
-                onChange={e => setData({...data, class:e.target.value})} required>
-                  <option value="NA">NA</option>
-                  <option value="CSE3A">CSE3A</option>
-                  <option value="CSE3B">CSE3B</option>
-                  <option value="CSE5A">CSE5A</option>
-                  <option value="CSE5B">CSE5B</option>
-              </select>    
+              <select className={styles.input}
+                onChange={e => setData({ ...data, class: e.target.value })} required>
+                <option value="NA">CLASS</option>
+                <option value="CSE3A">CSE3A</option>
+                <option value="CSE3B">CSE3B</option>
+                <option value="CSE5A">CSE5A</option>
+                <option value="CSE5B">CSE5B</option>
+              </select>
               <br />
 
               <button type="submit" className={styles.submitButton}>Submit <code>🏏</code></button>
@@ -108,10 +108,9 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-          
-          <a href="https://youtu.be/dG4DGVikMpU"><YouTube /></a>
-          <a href="www.linkedin.com/in/pylampofficial"><LinkedIn /></a>
-          <a href=""><Instagram /></a>
+        <a href="https://youtu.be/dG4DGVikMpU"><YouTube  style={{color:'white'}}/></a>
+        <a href="https://linkedin.com/in/pylampofficial"><LinkedIn style={{color:'white'}}/></a>
+        <a href="https://instagram.com/pylamp_official_"><Instagram style={{color:'white'}}/></a>
       </footer>
 
 
@@ -120,37 +119,37 @@ export default function Home() {
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
-        >
+      >
         <DialogTitle className={styles.alertDialogTitle}>
-            Well Done Folks!!!!
+          Well Done Folks!!!!
         </DialogTitle>
         <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-                Successfully your attendance marked..
-            </DialogContentText>
+          <DialogContentText id="alert-dialog-description">
+            Successfully your attendance marked..
+          </DialogContentText>
         </DialogContent>
         <DialogActions>
-        <Button onClick={routeAttendancePage} autoFocus>
+          <Button onClick={routeAttendancePage} autoFocus>
             Look🧐
-        </Button>
-        <Button onClick={handleClose} autoFocus>
+          </Button>
+          <Button onClick={handleClose} autoFocus>
             Close
-        </Button>
+          </Button>
         </DialogActions>
       </Dialog>
 
-      {/* <ul className={styles.circles}>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-            </ul> */}
+      <ul className={styles.circles}>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   )
 }

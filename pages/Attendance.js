9fd6cf84d51673ce00ca72de8c847f32ea45e7d2
Tharunkumar001 <tharung1411@gmail.com>
2021-12-21 21:00:06 +0530

    ui updation

--- pages/Attendance.js
+++ pages/Attendance.js
@@ -2,13 +2,14 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { ListItem, Tab, Table } from '@material-ui/core';
+import { Button, Card, IconButton, ListItem, Tab, Table } from '@material-ui/core';
 import { Grid } from '@material-ui/core';
-
+import BoxIcon from "@material-ui/icons/AddBoxOutlined"
+import ListIcon from "@material-ui/icons/ListAltOutlined";
 
 const TableData = (props) => {
     return(
-            <Grid name={props.name} section={props.section} roll={props.roll} container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+            <Grid name={props.name} section={props.section} roll={props.roll} container rowSpacing={1} columnSpacing={{ xs: 3, sm: 3, md: 3 }}>
                 <Grid item xs={2}>
                     <ListItem>{props.serialNo}</ListItem>
                 </Grid>
@@ -25,10 +26,13 @@ const TableData = (props) => {
 
 export default function Attendance() {
     const [data,setData] = useState([]);
+    const [view,setView] = useState("grid");
+
     var bool = false;
 
     useEffect(() => {
         axios.get("https://pylamp-domain-realm.vercel.app/api/formHandler").then((res) => {
+            //https://pylamp-domain-realm.vercel.app
             var arrayOfData = res.data;
             var sortedArray = arrayOfData.sort((a,b) => (a.class > b.class) ? 1 : ((b.class > a.class) ? -1 : 0))
             setData(sortedArray)
@@ -45,26 +49,29 @@ return (
         <h3>Domain Realm Attendance</h3>
 
         <div>
-            {data.map((ele,ind) => (
-                // <p key={_id}>Coffee type {name} </p>
-                <TableData key={ind} serialNo={ind+1} name={ele.name} section={ele.class} roll={ele.rollNo}/>
-            ))}
-        </div>
+            <Button variant="contained" startIcon={<BoxIcon />} onClick={() => setView("grid")}>
+                GridView
+            </Button>
+
+            <Button variant="contained" endIcon={<ListIcon />} onClick={() => setView("list")}>
+                ListView
+            </Button>
+        </div><br />
+        
+        
+            {(view == "grid")? <Grid container rowSpacing={1} spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                    {data.map((ele,ind) => (
+                            <Grid item xs={6} sm={4} md={4} key={ind} className={styles.AttendanceCard}>
+                                <h5>{ele.name}</h5>
+                                <h6>{ele.rollNo}</h6>
+                            </Grid>                       
+                    ))}
+            </Grid> : <Grid container rowSpacing={1} spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                    {data.map((ele,ind) => (
+                        <TableData key={ind} serialNo={ind+1} name={ele.name} section={ele.class} roll={ele.rollNo}/>
+                    ))}
+            </Grid> }
     </main>
-        <ul className={styles.circles}>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-            </ul>
     </div>
-
-
 )
 }
--- pages/index.js
+++ pages/index.js
@@ -108,9 +108,9 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a href="https://www.youtube.com/channel/UCJBaFNQuwfYXHDkICKpMYsg"><YouTube  style={{color:'white'}}/></a>
-        <a href="https://linkedin.com/in/pylampofficial"><LinkedIn style={{color:'white'}}/></a>
-        <a href="https://instagram.com/pylamp_official_"><Instagram style={{color:'white'}}/></a>
+        <a href="https://www.youtube.com/channel/UCJBaFNQuwfYXHDkICKpMYsg" target="blank"><YouTube  style={{color:'white'}}/></a>
+        <a href="https://linkedin.com/in/pylampofficial" target="blank"><LinkedIn style={{color:'white'}}/></a>
+        <a href="https://instagram.com/pylamp_official_" target="blank"><Instagram style={{color:'white'}}/></a>
       </footer>
 
 

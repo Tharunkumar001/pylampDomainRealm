a97823ddb51e67e8dfa96d69f47b114ef46d7047
Tharunkumar001 <tharung1411@gmail.com>
2022-01-08 00:09:57 +0530

    default form ui almost done

--- pages/FormContainer/DefaultForm.js
+++ pages/FormContainer/DefaultForm.js
@@ -5,7 +5,9 @@ import Logo from "../../public/pylampLogo.png";
 import Cover from "../../public/cover.jpg";
 import EdiText from 'react-editext';
 import { useState } from 'react';
-
+import YouTube from "@material-ui/icons/YouTube";
+import LinkedIn from "@material-ui/icons/LinkedIn";
+import Instagram from "@material-ui/icons/Instagram";
 
 export default function Final() {
     const [data, setData] = useState({ name: "", rollNo: "", class: "NA" });
@@ -26,17 +28,29 @@ return (
             <div className={styles.formHeader}>
                 <h1 style={{textAlign:"center"}}>Pylamp</h1>
                 <Image src={Logo} alt="logo" width="140rem" height="140rem" className={styles.profileImg}/>
-            </div><br /><br />
+            </div>
 
-            <h4 className={styles.period}>
-                <EdiText type="text" value={editContent.period} onSave={handleSave} />
+            <div className={styles.rowBtn} >
+              <h4 className={styles.period}>
                 <EdiText type="text" value={editContent.header} onSave={handleSave} />
+              </h4>
+
+              
+              <h4 className={styles.period}>
+                <EdiText type="text" value={editContent.period} onSave={handleSave} />
+              </h4>
+
+            </div>
+            <h4 className={styles.period}>
                 <EdiText type="text" value={editContent.about} onSave={handleSave} />
-            </h4><br />
+              </h4>
 
         <div className={styles.form}>
           <label>
+          <h2>🎯</h2>
+
             <form className={styles.formLabel}>
+
               <input placeholder="Name" autoComplete="off" value={data.name.trim()} type="text" name="inputForName" required className={styles.input}
                 onChange={e => setData({ ...data, name: e.target.value })}
               /><br />
@@ -60,6 +74,12 @@ return (
           </label>
         </div>
         </div>
+
+        <footer className={styles.homeFotter}>
+          <a href="https://www.youtube.com/channel/UCJBaFNQuwfYXHDkICKpMYsg" target="blank"><YouTube  style={{color:'white'}}/></a>
+          <a href="https://linkedin.com/in/pylampofficial" target="blank"><LinkedIn style={{color:'white'}}/></a>
+          <a href="https://instagram.com/pylamp_official_" target="blank"><Instagram style={{color:'white'}}/></a>
+        </footer>
     </div>
 )
 }

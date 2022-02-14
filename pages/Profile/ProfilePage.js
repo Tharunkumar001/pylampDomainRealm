6853d82f8ac550cb7cc6fbe5833e598ac5cba8d9
Tharunkumar001 <tharung1411@gmail.com>
2022-02-14 20:40:06 +0530

    bug fix

--- pages/Profile/ProfilePage.js
+++ pages/Profile/ProfilePage.js
@@ -1,25 +1,13 @@
 import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
 import { useRouter } from 'next/dist/client/router';
-import { Avatar, Button, Card, CardContent, CardHeader, Grid, Typography,  } from '@material-ui/core';
+import { Avatar, Button, Card, CardContent, CardHeader, Typography,  } from '@material-ui/core';
 import { ArrowForwardIos } from '@material-ui/icons';
 import { DataGrid } from '@material-ui/data-grid';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import cookie from 'react-cookies'
-import {CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
-
-import {
-    BarChart,
-    Bar,
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Tooltip,
-    Legend
-} from "recharts";
-
+import { Stack } from 'react-bootstrap';
 
 const columns = [
     {
@@ -46,28 +34,18 @@ const columns = [
 export default function ProfilePage() {
     const [row, setRow] = useState([]);
     const [user, setUser] = useState({userName: "", userRollNo: ""});
-    const [bar, setBar] = useState(data);
-    const [barData, setData] = useState({Participation:"1", Event: "1",});
-    const [Avg, setAvg] = useState(0);
     const router = useRouter();
-
-    const data = [
-        {
-            name: "Participation",
-            Participation:  `${barData.Participation}`,
-            Events: `${barData.Event}`,
-        },
-    ];
 
     useEffect(() => {
         (async() => {
             const jwtCall = await axios.put("https://pylamp-domain-realm.vercel.app/api/profileHandler",{jwt: cookie.load("jwt")})
+            const User = await axios.put("https://pylamp-domain-realm.vercel.app/api/profileApi",{rollNo: jwtCall.data.user})
+            setUser({...user, userName: User.data[0].UserName, userRollNo: User.data[0].RollNo});
+
             try {
                 const apiCall = await axios.post("https://pylamp-domain-realm.vercel.app/api/profileApi",{rollNo: jwtCall.data.user});
-                
                 var expRows = [];
-                let data = putCall.data.active;
-
+                let data = apiCall.data;
                 if(apiCall.status == 200){
                     data.map((value,index) => {
                         expRows.push(
@@ -76,21 +54,13 @@ export default function ProfilePage() {
                     });
                     setRow(expRows.reverse());
                 }
-
-                let eventVal = barData.Event;
-                let participation = barData.Participation;
-
-                var divide = (eventVal/participation);
-                var average = Math.floor(100/divide);
-
-                await setAvg(average);
             } catch (error) {
                 console.log(error)
             }
-
             
+
         })();
-    },[barData.Event, barData.Participation])
+    },[])
 return (
     <div>
         <Head>
@@ -132,51 +102,19 @@ return (
                         autoHeight={true}
                         autoPageSize={true}
                         checkboxSelection={false}
+                        components={{
+                            NoRowsOverlay: () => (
+                                <Stack height="100%" alignItems="center" justifyContent="center">
+                                    No rows in DataGrid
+                                </Stack>
+                            )
+                        }}
                     />
                 </CardContent>
 
                 <CardContent className={styles.profileSegment}>
                     Stats
                 </CardContent>
-
-                <CardContent id="myChart">
-                <Grid container spacing={2} >
-                <Grid item xs={12} md={8} sm={8}>
-                    <BarChart
-                            width={300}
-                            height={300}
-                            data={data}
-                        >
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="name" />
-                            <YAxis />
-                            <Tooltip />
-                            <Legend />
-                            <Bar dataKey="Events" fill="#8884d8" />
-                            <Bar dataKey="Participation" fill="#82ca9d" />
-                        </BarChart>
-                </Grid><br />
-
-                <Grid item xs={6} md={4} sm={4} style={{
-                    display:"flex",
-                    marginRight:"auto",
-                    marginLeft:"auto"
-                }}>
-                        <CircularProgressbar 
-                            strokeWidth={2} 
-                            value={Avg} 
-                            text={`
-                            ${Avg}%
-                            `}
-                            
-                        />
-                </Grid>  
-            </Grid>
-            </CardContent>
-
-            <CardContent>
-                <h4>Active % should greater than 40%</h4>
-            </CardContent>
             </Card>
         </div>
     </div>
